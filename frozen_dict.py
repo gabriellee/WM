bb1580758f288b2a1a25355fdc7a7edfcff37b1a
gabriellee <gabrielle.ewall@students.olin.edu>
2014-06-19 16:51:47 -0400

    frozen_dict now recursively freezes lists and dictionaries inside the argument.  Assignment of transition values appears successful.  I checked most of them.

--- frozen_dict.py
+++ frozen_dict.py
@@ -1,3 +1,6 @@
+import copy
+import pdb
+
 class frozendict(dict):
     def _blocked_attribute(obj):
         raise AttributeError, "A frozendict cannot be modified."
@@ -8,6 +11,54 @@ class frozendict(dict):
 
     def __new__(cls, *args, **kw):
         new = dict.__new__(cls)
+
+        args_ = []
+        for arg in args:
+            if isinstance(arg, dict):
+                arg = copy.copy(arg)
+                for k, v in arg.items():
+                    if isinstance(v, dict):
+                        arg[k] = frozendict(v)
+                    elif isinstance(v, list):
+                        v_ = list()
+                        for elm in v:
+                            if isinstance(elm, dict):
+                                v_.append( frozendict(elm) )
+                            else:
+                                v_.append( elm )
+                        arg[k] = tuple(v_)
+                args_.append( arg )
+            else:
+                args_.append( arg )
+
+        dict.__init__(new, *args_, **kw)
+        return new
+
+
+
+        # frozenargs=[]
+        # for arg in args:
+        # 	if isinstance(arg, dict):
+        #         arg = copy.copy(arg)
+        # 		for key, value in arg.items():
+        # 			if isinstance(value, dict):
+        # 				arg[key] = frozendict(value)
+        # 			elif isinstance(value, list):
+        # 				freezevalue = list()
+        # 				for element in value:
+        # 					if isinstance(element, dict):
+        # 						freezevalue.append(frozendict(element))
+        # 					else:
+        # 						freezevalue.append(element)
+        # 				arg[key] = tuple(freezevalue)
+        # 		frozenargs.append(arg)
+        # 	else:
+        # 		frozenargs.append(arg)
+
+        #dict.__init__(new, *args, **kw)
+        #return new
+
+
         dict.__init__(new, *args, **kw)
         return new
 
